25aff7ac0e13f7684bd0865f5b412dbb3a2b4a37
DevAlbert97 <dev.carloshudm@gmail.com>
2022-08-23 12:29:25 -0700

    dev commons and numbers pages

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
   styleUrls: ['./numbers.component.sass']
 })
-export class NumbersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumbersComponent{
+  netSales: number = 2567789.5567;
+  porcent: number = 0.4856;
 }
